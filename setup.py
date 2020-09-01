7a00d5702235bcb12ae52e6f779f8b7bfb5264d4
gp-atlassian <gpidcock@atlassian.com>
2020-09-01 22:10:59 +1000

    add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='AIS Hackathon 2020 Submission',
+    author='Civita PacifImpact',
+    license='MIT',
+)
